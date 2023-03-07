223cdfdf4e17b9a9d091a1d5d417008a66416a0c
brocode <kelimbth@gmail.com>
2023-03-07 16:18:01 +0300

    registered models in the admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Questions, Answers
 
-# Register your models here.
+@admin.register(Questions)
+class QuestionsTable(admin.ModelAdmin):
+    list_display = ['author', 'question', 'posted']
+
+@admin.register(Answers)
+class AnswersTable(admin.ModelAdmin):
+    list_display = ['author', 'answer', 'posted']
+    

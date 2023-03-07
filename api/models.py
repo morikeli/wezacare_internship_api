106c7c941ea6eb978d9144f368afd9ce148437e7
brocode <kelimbth@gmail.com>
2023-03-07 15:48:10 +0300

    created db tables

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Questions(models.Model):
+    id = models.CharField(max_length=10, primary_key=True, editable=False, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    question = models.TextField(blank=False)
+    posted = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Questions'
+        ordering = ['author']
+    
+    def __str__(self):
+        return f'{self.author}'
+
+
+class Answers(models.Model):
+    id = models.CharField(max_length=10, primary_key=True, editable=False, unique=True)
+    author = models.ForeignKey(Questions, on_delete=models.CASCADE, editable=False)
+    answer = models.TextField(blank=False)
+    posted = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Answers'
+        ordering = ['author']
+
+    def __str__(self):
+        return f'{self.author}'

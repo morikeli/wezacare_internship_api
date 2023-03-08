e7b00b270a9122a458e326228485636cd2d27c53
brocode <kelimbth@gmail.com>
2023-03-08 12:09:28 +0300

    added HTTP status

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from .serializers import QuestionsSerializer, AnswersSerializer
 from .models import Questions, Answers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
@@ -18,7 +19,7 @@ def get_all_questions_view(request):
         
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'DELETE'])
@@ -32,6 +33,7 @@ def get_selected_question_view(request, questionID):
 
     elif request.method == 'DELETE':
         quiz.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'POST'])
@@ -42,7 +44,7 @@ def post_answers_view(request, questionID):
         post_answer = Answers.objects.get(author_id=quiz)
         serializer = AnswersSerializer(post_answer)
 
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     elif request.method == 'POST':
         answer_obj = Answers.objects.get(author_id=quiz)
@@ -64,9 +66,9 @@ def update_answers_view(request, questionID, answerID):
             serializer.save()
             return Response(serializer.data)
         
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
         answ.delete()
-
+        return Response(status=status.HTTP_204_NO_CONTENT)
 

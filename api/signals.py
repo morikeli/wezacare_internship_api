5f982721750f9d31118be9e10eac260f8fc37e98
brocode <kelimbth@gmail.com>
2023-03-08 13:36:28 +0300

    added signal func. to generate user ID

--- api/signals.py
+++ api/signals.py
@@ -1,7 +1,14 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Questions, Answers
+from .models import Questions, Answers, User
 import uuid
+
+
+@receiver(pre_save, sender=User)
+def generate_userID(sender, instance, **kwargs):
+    if instance.id == "":
+        instance.id = str(uuid.uuid4()).replace('-', '').upper()[:10]
+
 
 
 @receiver(pre_save, sender=Questions)

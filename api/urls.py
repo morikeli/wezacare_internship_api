3b8a6c4a3d679c300a13adb0d39b5719df638b1b
brocode <kelimbth@gmail.com>
2023-03-08 13:57:12 +0300

    renamed app url link

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
+    path('register', views.signup_view, name=''),
     path('questions', views.get_all_questions_view, name='all_questions'),
     path('questions/<str:questionID>', views.get_selected_question_view, name='selected_question'),
     path('questions/<str:questionID>/answers', views.post_answers_view, name='post_users_answer'),
--- api/views.py
+++ api/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import get_object_or_404
-from .serializers import QuestionsSerializer, AnswersSerializer
+from .serializers import QuestionsSerializer, AnswersSerializer, UserSignupSerializer
 from .models import Questions, Answers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+
+@api_view(['POST'])
+def signup_view(request):
+    if request.method == 'POST':
+        serializer = UserSignupSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response()
 
 
 @api_view(['GET', 'POST'])

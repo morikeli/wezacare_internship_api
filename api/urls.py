78d2f5a2a629c7441615ac4327e8a20a7b60c935
brocode <kelimbth@gmail.com>
2023-03-08 11:58:09 +0300

    hotfix: corrected URL link typo error

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('questions', views.get_all_questions_view, name='all_questions'),
     path('questions/<str:questionID>', views.get_selected_question_view, name='selected_question'),
     path('questions/<str:questionID>/answers', views.post_answers_view, name='post_users_answer'),
-    path('question/<str:questionID>/answers/<str:answerID>', views.update_answers_view, name='update_answer'),
+    path('questions/<str:questionID>/answers/<str:answerID>', views.update_answers_view, name='update_answer'),
     
 
 ]

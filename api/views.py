c71abd61bcd5379e9ffe7d52a2f3a86f2df961fd
brocode <kelimbth@gmail.com>
2023-03-08 06:19:08 +0300

    added new views

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from .serializers import QuestionsSerializer, AnswersSerializers
+from .serializers import QuestionsSerializer, AnswersSerializer
 from .models import Questions, Answers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -10,6 +10,8 @@ def get_all_questions_view(request):
     if request.method == 'GET':
         questions = Questions.objects.all()
         serializer = QuestionsSerializer(questions, many=True)
+    
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         serializer = QuestionsSerializer(data=request.data)
@@ -17,8 +19,6 @@ def get_all_questions_view(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-    
-    return Response(serializer.data)
 
 
 @api_view(['GET', 'DELETE'])
@@ -28,9 +28,41 @@ def get_selected_question_view(request, questionID):
     if request.method == 'GET':
         serializer = QuestionsSerializer(quiz)
 
+        return Response(serializer.data)
+
     elif request.method == 'DELETE':
         quiz.delete()
 
 
-    return Response(serializer.data)
+@api_view(['GET', 'POST'])
+def post_answers_view(request, questionID):
+    quiz = get_object_or_404(Questions, id=questionID)
+
+    if request.method == 'GET':
+        post_answer = Answers.objects.get(author_id=quiz)
+        serializer = AnswersSerializer(post_answer)
+
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        answer_obj = Answers.objects.get(author_id=quiz)
+        serializer = AnswersSerializer(answer_obj, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(serializer.data)
+
+@api_view(['PUT'])
+def update_answers_view(request, questionID, answerID):
+    answ = Answers.objects.get(author_id=questionID)
+
+    if request.method == 'PUT':
+        serializer = AnswersSerializer(answ, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
 

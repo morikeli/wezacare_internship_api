aa0aa2dcc6f94e873ae0adafcba792187c952d48
brocode <kelimbth@gmail.com>
2023-03-08 13:33:04 +0300

    created new user db table

--- api/models.py
+++ api/models.py
@@ -1,5 +1,12 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, blank=False)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Questions(models.Model):

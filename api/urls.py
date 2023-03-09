0fc9627ed1e9931863387ed1e7b0e59026bfcd7d
brocode <kelimbth@gmail.com>
2023-03-09 21:13:44 +0300

    edited logout and questions URL paths

--- api/urls.py
+++ api/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('auth/login', views.LoginView.as_view(), name='user_login'),
     path('auth/register', views.SignupView.as_view(), name='signup'),
-    path('questions', views.get_all_questions_view, name='all_questions'),
+    path('questions', views.QuestionsView.as_view(), name='all_questions'),
     path('questions/<str:questionID>', views.get_selected_question_view, name='selected_question'),
     path('questions/<str:questionID>/answers', views.post_answers_view, name='post_users_answer'),
     path('questions/<str:questionID>/answers/<str:answerID>', views.update_answers_view, name='update_answer'),
-    path('logout/', views.logout_user_view, name='user_logout'),
+    path('logout/', views.LogoutUserView.as_view(), name='user_logout'),
     
 
 ]

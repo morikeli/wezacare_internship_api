b6a70c999b8e805232a78b837fad5d6ebe965fe0
brocode <kelimbth@gmail.com>
2023-03-09 23:05:25 +0300

    updated URL path

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('auth/register', views.SignupView.as_view(), name='signup'),
     path('questions', views.QuestionsView.as_view(), name='all_questions'),
     path('questions/<str:questionID>', views.get_or_delete_QuestionsView.as_view(), name='selected_question'),
-    path('questions/<str:questionID>/answers', views.post_answers_view, name='post_users_answer'),
+    path('questions/<str:questionID>/answers', views.SendAnswersView.as_view(), name='post_answer'),
     path('questions/<str:questionID>/answers/<str:answerID>', views.update_answers_view, name='update_answer'),
     path('logout/', views.LogoutUserView.as_view(), name='user_logout'),
     

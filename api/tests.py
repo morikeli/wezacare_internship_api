d9b3c7c7ef985ea6ee6dd3667e5e988cd0475630
brocode <kelimbth@gmail.com>
2023-03-11 13:20:26 +0300

    added testcase methods and class

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TestSetup(APITestCase):
+    def setUp(self):
+        self.register_url = reverse('signup')
+        self.login_url = reverse('user_login')
+
+        self.user_data = {
+            "email": "sarahj@gmail.com",
+            "username": "sarahj",
+            "password": "morikeli",
+        }
+
+        return super().setUp()
+
+    def tearDown(self):
+        return super().tearDown()
+
+
+class TestView(TestSetup):
+    def test_user_signup(self):
+        res = self.client.post(self.register_url, self.user_data)
+        self.assertEqual(res.status_code, 201)
+
+    def test_user_can_login(self):
+        res = self.client.post(self.login_url, self.user_data, format='json')
+        self.assertEqual(res.status_code, 401)

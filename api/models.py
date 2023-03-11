953b073810de4b3f71689ef59a6b219dacdbe44c
brocode <kelimbth@gmail.com>
2023-03-11 14:02:49 +0300

    added dosumentation strings

--- api/models.py
+++ api/models.py
@@ -12,6 +12,15 @@ class User(AbstractUser):
 
 
 class Questions(models.Model):
+    """
+        A given user can ask many questions. ForeignKey class is used to establish one-to-many relationship.
+        In this case, one user can ask many questions (one-to-many relationship).
+        Fields:
+            - author: user who posted the question.
+            - question: the question asked.
+            - posted: date and time when the question was posted.
+            - edited: date and time the author of the question updated the question
+    """
     id = models.CharField(max_length=10, primary_key=True, editable=False, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
     question = models.TextField(blank=False)
@@ -27,6 +36,16 @@ class Questions(models.Model):
 
 
 class Answers(models.Model):
+    """
+        A given user can post many answers. ForeignKey class is used to establish one-to-many relationship.
+        In this case, one user can ask many answers (one-to-many relationship).
+        Fields:
+            - author: the question asked.
+            - answered_by: user who posted the answer to a given question.
+            - answer: the answer asked.
+            - posted: date and time when the answer was posted.
+            - edited: date and time the author updated the his/her response.
+    """
     id = models.CharField(max_length=10, primary_key=True, editable=False, unique=True)
     author = models.ForeignKey(Questions, on_delete=models.CASCADE, editable=False)
     answered_by = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
--- api/signals.py
+++ api/signals.py
@@ -6,6 +6,11 @@ import uuid
 
 @receiver(pre_save, sender=User)
 def generate_userID(sender, instance, **kwargs):
+    """
+        By default django assigns users with IDs base on AutoField.
+        This function overides this system by generating IDs using uuid numbers
+        with a 10 characters.
+    """
     if instance.id == "":
         instance.id = str(uuid.uuid4()).replace('-', '').upper()[:10]
 
@@ -13,12 +18,18 @@ def generate_userID(sender, instance, **kwargs):
 
 @receiver(pre_save, sender=Questions)
 def generate_questionID(sender, instance, **kwargs):
+    """
+        Generate IDs using uuid numbers by manipulating uuid module. Each question has unique IDs.
+    """
     if instance.id == "":
         instance.id = str(uuid.uuid4()).replace('-', '').upper()[:10]
 
 
 @receiver(pre_save, sender=Answers)
 def generate_answersID(sender, instance, **kwargs):
+    """
+        Generate IDs using uuid numbers by manipulating uuid module. Each answer has unique IDs.
+    """
     if instance.id == "":
         instance.id = str(uuid.uuid4()).replace('-', '').upper()[:10]
 

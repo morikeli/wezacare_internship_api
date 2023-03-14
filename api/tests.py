ff7886a4cb988bec052b152e0f21b17beb5c3010
brocode <kelimbth@gmail.com>
2023-03-14 16:57:50 +0300

    added signup test method

--- api/tests.py
+++ api/tests.py
@@ -1,28 +1,35 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
+from .models import User
+import jwt
+from datetime import datetime, timedelta
 
 class TestSetup(APITestCase):
     def setUp(self):
         self.register_url = reverse('signup')
         self.login_url = reverse('user_login')
+        self.questions_url = '/questions/'+"5BAD9524B2"
 
         self.user_data = {
-            "email": "sarahj@gmail.com",
-            "username": "sarahj",
+            "email": "testuser@gmail.com",
+            "username": "testuser",
             "password": "morikeli",
+            # "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjNDQzExNEQ4NkEiLCJleHAiOjE2Nzg4MDM5MzksImlhdCI6MTY3ODgwMDMzOX0.\
+            # gfrtNLFOkYEslJbOX99IWXTCZT4OMpvgWvnok2I6lkI"
+            
         }
 
+        self.questions_data = {
+            "id": "5BAD9524B2",
+            "author": "3CC114D86A",
+            "question": "Question 2",
+        }
+    
         return super().setUp()
 
-    def tearDown(self):
-        return super().tearDown()
-
-
-class TestView(TestSetup):
-    def test_user_signup(self):
+    def test_signup(self):
         res = self.client.post(self.register_url, self.user_data)
+        import pdb
+        pdb.set_trace()
         self.assertEqual(res.status_code, 201)
-
-    def test_user_can_login(self):
-        res = self.client.post(self.login_url, self.user_data, format='json')
-        self.assertEqual(res.status_code, 401)
+    
